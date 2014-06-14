18275e1d54ae7eb55d2732fdf6ae0c37acff56e7
filipabramowicz <filip.abramowicz@gmail.com>
2014-06-14 18:42:31 +0200

    File handling implemented

--- http_data_parser/get.js
+++ http_data_parser/get.js
@@ -1,7 +1,8 @@
 // Include http module
 var http = require('http'),
 // And url module which is very helpful in parsing request parameters
-	url = require('url');
+	url = require('url'),
+	fs = require('fs');
 
 // Create the server
 http.createServer(function(request, response){
@@ -9,12 +10,19 @@ http.createServer(function(request, response){
 	// This function parses the url from request and returns object representation
 	if (request.url != '/')
 	{
-		var _get = url.parse(request.url, true).query;
-		response.writeHead(200, {
-			'Content-Type': 'text/plain'
+		fs.readFile('test.txt', 'utf-8', function(error, data){
+			var _get = url.parse(request.url, true).query;
+			response.writeHead(200, {
+				'Content-Type': 'text/plain'
+			});
+			// Increment the number obtained from file
+			data = parseInt(data) + 1;
+			// Write incremented number to file
+			fs.writeFile('test.txt', data)
+			// Send data and end response
+			response.end('This page was refreshed ' + data + ' times. '+'Here is your data: ' + _get['data']);
 		});
-		// Send data and end response
-		response.end('Here is your data: ' + _get['data']);
+
 	} else {
 		response.writeHead(200, {
 			'Content-Type': 'text/plain'
